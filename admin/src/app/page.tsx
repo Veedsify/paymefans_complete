00a8ef990e478e7ef1e551d90b43938bc17b8406
= <=>
2024-06-09 13:42:59 +0100

    chore: Update admin panel layout and button text for better user experience

--- admin/src/app/page.tsx
+++ admin/src/app/page.tsx
@@ -5,14 +5,14 @@ import DashBoardTab from "@/page-components/dashboard/data-tabs";
 export default function Home() {
   return (
     <>
-        <div className="flex items-center justify-between space-y-2">
-          <h2 className="text-3xl font-bold tracking-tight">Admin Panel</h2>
-          <div className="flex items-center space-x-2">
-            <CalendarDateRangePicker />
-            <Button>Download</Button>
-          </div>
+      <div className="flex items-center justify-between space-y-2">
+        <h2 className="text-3xl font-bold tracking-tight">Admin Panel</h2>
+        <div className="flex items-center space-x-2">
+          <CalendarDateRangePicker />
+          <Button>Download</Button>
         </div>
-        <DashBoardTab />
+      </div>
+      <DashBoardTab />
     </>
   );
 }
